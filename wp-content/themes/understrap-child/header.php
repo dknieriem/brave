312bc9a24bbcd4ea2f6761da464bd753ff35996a
Donald Knieriem <don@thecanarium.com>
2020-04-09 21:05:42 -0400

    rev1

--- wp-content/themes/understrap-child/header.php
+++ wp-content/themes/understrap-child/header.php
@@ -20,7 +20,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Bebas+Neue&family=Oswald:wght@300;400&family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Bebas+Neue&family=Oswald:wght@300;400;500;600&family=Ubuntu:wght@300;400&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 	<!-- Google Tag Manager -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
