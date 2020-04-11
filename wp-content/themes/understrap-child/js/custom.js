06554fd6cd62fff2683d9b617b32e2f316cf96f2
Donald Knieriem <don@thecanarium.com>
2020-04-10 21:27:45 -0400

    rev2

--- wp-content/themes/understrap-child/js/custom.js
+++ wp-content/themes/understrap-child/js/custom.js
@@ -18,13 +18,20 @@ jQuery('.blog__sidebar').hcSticky({
   }
 });
 
-jQuery(document).ready(function()
+var resetForm = function()
 {
   jQuery('#toolset-maps-distance-center')
-  	.attr('placeholder','Your Location or Zip Code');
+    .attr('placeholder','Your Location or Zip Code');
 
   jQuery('.events-map .wpv-filter-form .form-group')
   .contents().filter(function() { 
-  	return this.nodeType == Node.TEXT_NODE; })
+    return this.nodeType == Node.TEXT_NODE; })
   .remove();
-});
+
+  jQuery('.events-map .wpv-filter-form .js-toolset-maps-distance-current-location')
+  .remove();
+
+}
+
+
+jQuery(document).ready(resetForm);
